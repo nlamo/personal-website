b77bc81ce0387f2e3e64ff01adace64bd4fea7a8
Nick <lamothe.dev@gmail.com>
2021-07-21 20:33:37 -0400

    self-portrait added, responsiveness changes, minor stylistic change in contact form

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ New design - personal portfolio website.
 
 TODO:
 
-- Perhaps completely *rebuild* the 'About' section.
 - Experiment with gradients for the background colour.
-- Create multiple self-portraits, insert them into the relevant sub-grid in 'About', and then use CSS animations to make them 'animated' on hover.
+- Make 'About' section self-portrait into a CSS animation, experiment with variations.
 - Continue to refine the basic structure/styling of the grids and sections.
 - Polish the footer and try and resolve some difficulties in the interaction with the main section.
 - Fix the zoom issue for the index page image/animation.
--- about.php
+++ about.php
@@ -52,17 +52,14 @@
         <div class="about-container">
             <div class="about-grid">
 
-                <div class="portrait-sub-grid">
-                    <div class="portrait">Portrait 1</div>
-                    <div class="portrait">Portrait 2</div>
-                    <div class="portrait">Portrait 3</div>
-                    <div class="portrait">Portrait 4</div>
+                <div class="portrait">
+                    <img src="img/portrait1.jpg" alt="">
                 </div>
 
                 <div class="about-me">
                     <p>
                         My passion for technology started at an early age - from troubleshooting issues in MS-DOS with my father to using the terminal of a Commodore 64 for the first time in one of the school portables. This developed concurrently with an obsession with gaming, which sparked an interest in hardware and building desktop computers.<br></br>
-    
+
                         Eventually, I became enthralled by the study of philosophy and literature, with a special interest in philosophy of mind, ethics, and modernism, thus forming the basis of my undegraduate studies. Years later, by way of literary insight, I was compelled to start programming.<br></br>
                         
                         I'm currently finishing up a post-bacc graduate diploma in computer science while studying web development and UX in my spare time. I also love to cycle and illustrate, and I even like to try my hand at a few algorithms, here and there.<br><br>
@@ -70,7 +67,6 @@
                         You can find my resume <a href="cv/Nicholas_LaMothe_CV.pdf">here</a>. 
                     </p>
                 </div>
-            </div>
         </div>
     </section>
 
