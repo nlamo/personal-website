ac442d9647848c9f303de5c558e63cdea1b49db4
nlamo <lamothe.dev@gmail.com>
2021-09-29 16:35:00 -0700

    tiny changes to file types

--- index.php
+++ index.php
@@ -57,11 +57,11 @@
 
             <img class="bottom-drawing" id="bottom-drawing" src="img/landscape_coloured.JPG" alt="">
             <img class="bottom-drawing-smartphone" id="bottom-drawing-smartphone" src="img/landscape_coloured_smartphone.JPG" alt="">
-            <img class="bottom-drawing-wide" id="bottom-drawing-wide" src="img/landscape_coloured_wide.JPG" alt="">
+            <img class="bottom-drawing-wide" id="bottom-drawing-wide" src="img/landscape_coloured_wide.jpg" alt="">
 
             <img class="top-drawing" id="top-drawing" src="img/landscape_outline.JPG" alt="">
-            <img class="top-drawing-smartphone" id="top-drawing-smartphone" src="img/landscape_outline_smartphone.JPG" alt="">
-            <img class="top-drawing-wide" id="top-drawing-wide" src="img/landscape_outline_wide.JPG" alt="">
+            <img class="top-drawing-smartphone" id="top-drawing-smartphone" src="img/landscape_outline_smartphone.jpg" alt="">
+            <img class="top-drawing-wide" id="top-drawing-wide" src="img/landscape_outline_wide.jpg" alt="">
 
             <div class="speech-bubble" id="speech-bubble">Greetings! I'm Nick. Welcome to my website. </div>
         </div>
