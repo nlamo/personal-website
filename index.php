3023f873d92c708791398803596c6decdc1f84a5
Nick <lamothe.dev@gmail.com>
2021-11-28 15:56:11 -0500

    some more 'responsive' changes

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="height=device-height, initial-scale=1, maximum-scale=1" />
     <link href="https://fonts.googleapis.com/css2?family=Sen&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap" rel="stylesheet">
