d350cad6dc9f03f24cc9092ff10922bd9fbb0e13
Nick <lamothe.dev@gmail.com>
2023-06-27 13:26:24 -0700

    scss/html tweaks, specifically for the footer

--- lib/footer.php
+++ lib/footer.php
@@ -11,13 +11,18 @@ class Footer
 			<<<HTML
 			   <footer>
 					<div class="footer-container">
-						<a href="https://www.linkedin.com/in/nicholas-lamothe/" target="_blank">
-							<img src="img/linkedin_logo.png">
-						</a>
-						<a href="https://github.com/nlamo/" target="_blank">
-							<img src="img/github_logo.png">
-						</a>
-
+						<div class="link-container">
+							<div class="link">
+								<a href="https://www.linkedin.com/in/nicholas-lamothe/" target="_blank">
+									<img src="img/linkedin_logo.png">
+								</a>
+							</div>
+							<div class="link">
+								<a href="https://github.com/nlamo/" target="_blank">
+									<img src="img/github_logo.png">
+								</a>
+							</div>
+						</div>
 						<div class="copyright-overlay">
 							&copy; <span id="current-year"></span> Nicholas LaMothe
 						</div>
